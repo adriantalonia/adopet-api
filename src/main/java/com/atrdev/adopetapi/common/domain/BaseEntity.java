30d665738018820f3493a242ec39619bec215fe1
Adrian Talonia <adrian.talonia11@gmail.com>
2025-10-30 22:38:51 -0600

    initial base entity

---
+++ src/main/java/com/atrdev/adopetapi/common/domain/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.atrdev.adopetapi.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    protected UUID id;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    protected Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    protected Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
